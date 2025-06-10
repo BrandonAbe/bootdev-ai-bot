05e8e480e52b0075207978bc9d51109872888ba2
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2025-06-10 18:03:46 -0400

    Added sys.argv support

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    prompt = client.models.generate_content(model="gemini-2.0-flash-001",contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-    print(f"Prompt tokens: {prompt.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {prompt.usage_metadata.candidates_token_count}")
-    print("Response:")
-    print(prompt.text)
+    if len(sys.argv) > 1:
+        prompt = client.models.generate_content(model="gemini-2.0-flash-001",contents=sys.argv[1])
+        print(f"Prompt tokens: {prompt.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {prompt.usage_metadata.candidates_token_count}")
+        print("Response:")
+        print(prompt.text)
+    else:
+        print("No arguments provided.")
 
 if __name__ == "__main__":
     main()

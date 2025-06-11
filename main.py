937dff76a86fe3dba1599cae9d1b5c3fe7e9f33f
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2025-06-10 23:28:48 -0400

    Added --verbose argv flag logic

--- main.py
+++ main.py
@@ -4,15 +4,25 @@ from google import genai
 from google.genai import types
 from dotenv import load_dotenv
 
+def generate_content(client, messages, verbose):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
+    print(response.text)
+
 
 def main():
     load_dotenv()
-
-    args = sys.argv[1:]
-
+    verbose = "--verbose" in sys.arg
+    args = [arg for arg in sys.argv[1:] if not arg.startswith("--")] # List Comprehension
     if not args:
         print("AI Code Assistant")
-        print('\nUsage: python main.py "your prompt here"')
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
         print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
     user_prompt = " ".join(args)
@@ -22,21 +32,14 @@ def main():
 
     user_prompt = " ".join(args)
 
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")
+
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    generate_content(client, messages)
-
-
-def generate_content(client, messages):
-    response = client.models.generate_content(
-        model="gemini-2.0-flash-001",
-        contents=messages,
-    )
-    print("Response:")
-    print(response.text)
-
+    generate_content(client, messages, verbose)
 
 if __name__ == "__main__":
     main()

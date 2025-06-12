34d5dc36de4ddd0f5212bd2c71b066829f5958a5
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2025-06-11 22:40:57 -0400

    Fixed path bug which lead to unexpected error when calling tests.py from root folder

--- functions/get_files_content.py
+++ functions/get_files_content.py
@@ -2,9 +2,9 @@ import os
 
 def get_file_content(working_directory:str, file_path:str):
     abs_working_dir = os.path.abspath(working_directory)
-    abs_file_path = os.path.abspath(file_path)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
 
-    if not abs_file_path.startswith(working_directory):
+    if not abs_file_path.startswith(abs_working_dir):
         return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
     
     if not os.path.isfile(abs_file_path):

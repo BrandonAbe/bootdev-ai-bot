6c61fcb679c7e097d6bba95924ffb5f43addefa0
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2025-06-11 22:46:23 -0400

    Refactor tests per instructions of Ch2_L3

--- tests.py
+++ tests.py
@@ -1,10 +1,22 @@
 from functions.get_files_content import get_file_content
 
 def test():
-    result = get_file_content("calculator", "lorem.txt")
+    result = get_file_content("calculator", "main.py")
     print("Result for current directory:")
     print(result)
     print("")
+
+    result = get_file_content("calculator", "pkg/calculator.py")
+    print("Result for current directory:")
+    print(result)
+    print("")
+
+    result = get_file_content("calculator", "/bin/cat")
+    print("Result for current directory:")
+    print(result)
+    print("")
+
+
 
 if __name__ == "__main__":
     test()

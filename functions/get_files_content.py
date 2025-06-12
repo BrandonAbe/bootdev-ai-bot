353883d2f2757cba1f4c4e95f46fc0825bab84c1
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2025-06-11 21:26:42 -0400

    Inital commit of get_files_content

---
+++ functions/get_files_content.py
@@ -0,0 +1,20 @@
+import os
+
+def get_file_content(working_directory:str, file_path:str):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(file_path)
+
+    if not abs_file_path.startswith(working_directory):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    
+    if not os.path.isfile(abs_file_path):
+        f'Error: File not found or is not a regular file: "{file_path}"'
+
+    try:
+        with open(abs_file_path, 'r', encoding='utf-8') as file:
+            content = file.read()
+            if len(content) > 10000:
+                content = content[:10000] + f'\n[...File "{file_path}" truncated at 10000 characters]'
+            return content
+    except Exception as e:
+        return f'Error: {str(e)}'

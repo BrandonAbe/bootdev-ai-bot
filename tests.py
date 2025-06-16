383787c8aa6302cba6aee3de3ea707437eb8767d
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2025-06-15 21:14:58 -0400

    Refactor to test run_python_file.py

--- tests.py
+++ tests.py
@@ -1,23 +1,22 @@
-from functions.get_files_content import get_file_content
-from functions.write_file import write_file
+from functions.run_python_file import run_python_file
+
 
 def test():
-    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
-    print("Result for current directory:")
+    print("First Test:")
+    result = run_python_file("calculator", "main.py")
     print(result)
-    print("")
 
-    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
-    print("Result for current directory:")
+    print("Second Test:")
+    result = run_python_file("calculator", "tests.py")
     print(result)
-    print("")
 
-    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
-    print("Result for current directory:")
+    print("Third Test:")
+    result = run_python_file("calculator", "../main.py")
     print(result)
-    print("")
 
-
+    print("Fourth Test:")
+    result = run_python_file("calculator", "nonexistent.py")
+    print(result)
 
 
 if __name__ == "__main__":

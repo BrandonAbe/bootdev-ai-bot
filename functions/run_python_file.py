ef10997d75347c452e6886836675aef57c913c66
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2025-06-15 21:20:26 -0400

    Fixed typo if unable to execute due to being outside working directory

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -9,7 +9,7 @@ def run_python_file(working_directory:str, file_path:str, args=None):
     abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
 
     if not abs_file_path.startswith(abs_working_dir):
-        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
     
     if not os.path.exists(abs_file_path):
         return f'Error: File "{file_path}" not found.'

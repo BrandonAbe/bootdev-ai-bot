5c8ada86b0362fe8e1c61210b99e10024e8a0ab2
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2025-06-15 21:19:00 -0400

    Remove print statements

--- tests.py
+++ tests.py
@@ -2,19 +2,15 @@ from functions.run_python_file import run_python_file
 
 
 def test():
-    print("First Test:")
     result = run_python_file("calculator", "main.py")
     print(result)
 
-    print("Second Test:")
     result = run_python_file("calculator", "tests.py")
     print(result)
 
-    print("Third Test:")
     result = run_python_file("calculator", "../main.py")
     print(result)
 
-    print("Fourth Test:")
     result = run_python_file("calculator", "nonexistent.py")
     print(result)
 

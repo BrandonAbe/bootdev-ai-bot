b54072f36e0088dbac36f2c8e42392a8f03858ca
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2025-09-18 23:28:16 -0400

    Add schema for Gemini calls

--- functions/get_files_content.py
+++ functions/get_files_content.py
@@ -18,3 +18,18 @@ def get_file_content(working_directory:str, file_path:str):
             return content
     except Exception as e:
         return f'Error: {str(e)}'
+    
+    schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description=f"Reads and returns the first {MAX_CHARS} characters of the content from a specified file within the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file whose content should be read, relative to the working directory.",
+            ),
+        },
+        required=["file_path"],
+    ),
+)

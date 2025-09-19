b3df2527c142a3a7e0e6bad45d38e39d8c2fcce4
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2025-09-18 23:49:57 -0400

    Bug fix: improper indentation

--- functions/get_files_content.py
+++ functions/get_files_content.py
@@ -1,5 +1,7 @@
 import os
 from config import MAX_CHARS
+from google.genai import types
+
 
 def get_file_content(working_directory:str, file_path:str):
     abs_working_dir = os.path.abspath(working_directory)
@@ -8,19 +10,18 @@ def get_file_content(working_directory:str, file_path:str):
     if not abs_file_path.startswith(abs_working_dir):
         return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
     if not os.path.isfile(abs_file_path):
-        f'Error: File not found or is not a regular file: "{file_path}"'
+        return f'Error: File not found or is not a regular file: "{file_path}"'
     try:
         with open(abs_file_path, 'r', encoding='utf-8') as file:
             content = file.read(MAX_CHARS)
             if os.path.getsize(abs_file_path) > MAX_CHARS:
-                content += (
-                    f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
-                )
+                content += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
             return content
     except Exception as e:
         return f'Error: {str(e)}'
-    
-    schema_get_file_content = types.FunctionDeclaration(
+
+
+schema_get_file_content = types.FunctionDeclaration(
     name="get_file_content",
     description=f"Reads and returns the first {MAX_CHARS} characters of the content from a specified file within the working directory.",
     parameters=types.Schema(

b20027360c1662a9a0c764e4842b8e1048644a48
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2025-09-22 23:50:46 -0400

    Add logic if function name IS in dictionary of allowed functions

--- call_function.py
+++ call_function.py
@@ -13,12 +13,15 @@ def call_function(function_call_part, verbose=False):
         "write_file": write_file,
     }
     function_name = function_call_part.name
-    args = dict(function_call_part.args)
+    
     if verbose:
         print(f"Calling function: {function_call_part.name}({function_call_part.args})")
     else:
         print(f" - Calling function: {function_call_part.name}")
     
+    args = dict(function_call_part.args)
+    args['working_directory'] = WORKING_DIR # Manually add working dir to dictionary
+    function_result = function_map[function_name](**args) # Unpack args dictionary into function parameters
     if function_name not in function_map:
         return types.Content(
             role="tool",
@@ -29,9 +32,15 @@ def call_function(function_call_part, verbose=False):
                 )
             ],
         )
-    
-    
-
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=function_name,
+                response={"result": function_result},
+            )
+        ],
+    )
 
 
 available_functions = types.Tool(

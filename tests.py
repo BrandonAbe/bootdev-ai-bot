cd594b96184943163267abc6fd16d24499984891
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2025-09-23 23:26:56 -0400

    Update unit tests based on requirements of Chapter 3 Lesson 4 Part 6

--- tests.py
+++ tests.py
@@ -2,17 +2,12 @@ from functions.run_python_file import run_python_file
 
 
 def test():
-    result = run_python_file("calculator", "main.py")
-    print(result)
-
-    result = run_python_file("calculator", "tests.py")
-    print(result)
-
-    result = run_python_file("calculator", "../main.py")
-    print(result)
-
-    result = run_python_file("calculator", "nonexistent.py")
-    print(result)
+    
+    print(run_python_file("calculator", "main.py"))
+    print(run_python_file("calculator", "main.py", ["3 + 5"]))
+    print(run_python_file("calculator", "tests.py"))
+    print(run_python_file("calculator", "../main.py"))
+    print(run_python_file("calculator", "nonexistent.py"))
 
 
 if __name__ == "__main__":
